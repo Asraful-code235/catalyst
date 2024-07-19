5fc6e88f706713471b1cfb33131dd7efec7bbdd8
olliedarby <oliverdarby99@gmail.com>
2024-07-19 20:31:31 +0100

    inital commit

---
+++ apps/catalyst-ui/src/components/organisms/footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="fixed bottom-0 border-gray-900 px-16 pt-10 pb-8">
+
+        </div>
+    )
+}
+
+export default Footer;
---
+++ apps/catalyst-ui/src/components/organisms/header/index.tsx
@@ -0,0 +1,16 @@
+const routes = [
+    {
+        title: '',
+        link: ''
+    }
+]
+
+const Header = () => {
+    return (
+        <div className="sticky top-0 z-50 h-12 border-b border-gray-900">
+            <div>test</div>
+        </div>
+    )
+}
+
+export default Header;
